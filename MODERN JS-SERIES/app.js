2b31218cf318dd3b5228e68ae6327cad32f4ee2e
jysleyin <julissaliangg@gmail.com>
2021-02-04 12:39:21 -0500

    Create app.js

---
+++ MODERN JS-SERIES/app.js
@@ -0,0 +1,36 @@
+// var life = 100; // Okay.
+// var name = "Mr Whalen";
+// var isSad = true;
+// var isHappy = null; // comes
+
+// life = life - 10;
+// // 90 hp
+// life = life - 10;
+// // 80 hp
+
+// console.log(life);
+// console.log(isSad);
+// console.log(isHappy);
+// // console.log("Hey Mr Whalen");
+
+// // constant means you cannot change the value
+// // const life = 100;
+// // life = life - 10;
+// // this cannot work.
+// // usage: select an image
+
+// stay consistent. use all double quotes or one
+console.log("Hell it's me Julissa");
+console.log("Hello it's me Julissa");
+
+const name = "Julissa";
+const age = 16;
+console.log(
+  "Hello it's me " + name + " and my age is " + age + " and I am old."
+);
+
+console.log("Hello its me $(name) and my age is $(age)");
+
+const combined = name + age;
+console.log(combined);
+console.log(typeof age);
